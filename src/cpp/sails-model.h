f4fbae1d02fdc83160e45b1f58f3a7853d98e647
Jordan Dialpuri <44945647+Dialpuri@users.noreply.github.com>
2024-02-17 16:21:37 +0000

    Refine sails-model.h with NAG_NAG struct and ResidueMap
    Removed large portions of unused and commented-out code in sails-model.h. Added a new struct NAG_NAG extending LinkageSet and altered ResidueType enum to include NAG. Also, adjusted the existing AminoAcidMap to ResidueMap to include NAG. These changes simplify the codebase and enhance our support for NAG residues.

--- src/cpp/sails-model.h
+++ src/cpp/sails-model.h
@@ -128,170 +128,46 @@ namespace Sails {
     
     };
 
+    struct NAG_NAG : LinkageSet {
+        DonorSet donor = NAG::donors()[4];
+        AcceptorSet acceptor = NAG::acceptors()[1];
+        LinkageParams params = LinkageParams(-80, -127, 180,
+                                             105, 120, 105, 1.45);
 
-    enum AminoAcidType {
-        ASN
+        NAG_NAG(const clipper::MMonomer& donor_residue, const clipper::MMonomer& acceptor_residue) {
+            m_transformation = calculate(donor_residue, acceptor_residue, donor, acceptor, params);
+            m_donor_residue = donor_residue;
+            m_acceptor_residue = acceptor_residue;
+        }
+
+        clipper::RTop_orth update_transformation(LinkageParams& parameters) override {
+            return calculate(m_donor_residue, m_acceptor_residue, donor, acceptor, parameters);
+        }
+
+        clipper::RTop_orth get_transformation() const {return m_transformation;}
+
+        clipper::MMonomer get_donor_monomer() override {return m_donor_residue;}
+
+        LinkageParams get_parameters() const override  {return params;}
+
+        void set_parameters(LinkageParams& parameters) override  { params = parameters;}
+
+    private:
+        clipper::RTop_orth m_transformation;
+        clipper::MMonomer m_donor_residue;
+        clipper::MMonomer m_acceptor_residue;
+
     };
 
-    static std::map<std::string, AminoAcidType> AminoAcidMap = {{"ASN", AminoAcidType::ASN}};
-    //
-    // /**
-    //  * \class AminoAcid
-    //  * \brief Represents an amino acid in Sails.
-    //  *
-    //  * The SailsAminoAcid class contains the information about an amino acid in Sails.
-    //  * After construction, it calculates the the two joining sugar atom positions defined by the psi and phi angles of an ASN-pyranose linkage
-    //  * https://doi.org/10.1107%2FS2059798323003510
-    //  */
-    // class AminoAcid {
-    // public:
-    //     /**
-    //      * @brief SailsAminoAcid constructor.
-    //      * @param amino_acid The reference to the amino acid MMonomer.
-    //      * @param atom_1 The name of the first atom in the phi/psi definition.
-    //      * @param atom_2 The name of the second atom in the phi/psi definition.
-    //      * @param atom_3 The name of the third atom in the phi/psi definition.
-    //      * @param phi_angle The angle value between the terminal aa atom, first atom of sugar and second atom of sugar.
-    //      * @param psi_angle The angle value between the penultimate aa atom, terminal aa atom and first atom of sugar.
-    //      * @param phi_torsion The torsion value phi torsion angle.
-    //      * @param psi_torsion The torsion value psi torsion angle.
-    //      */
-    //     explicit AminoAcid(clipper::MMonomer&amino_acid,
-    //                        const std::string&atom_1,
-    //                        const std::string&atom_2,
-    //                        const std::string&atom_3,
-    //                        float phi_angle,
-    //                        float psi_angle,
-    //                        float phi_torsion,
-    //                        float psi_torsion);
-    //
-    //     virtual ~AminoAcid() = default;
-    //
-    // protected:
-    //     /**
-    //      * @brief First atom of pyranose sugar that would be linked to this amino acid, for ASN-NAG position1 would represent C1.
-    //      */
-    //     clipper::Coord_orth position1;
-    //     /**
-    //     * @brief Second atom of pyranose sugar that would be linked to this amino acid, for ASN-NAG position2 would represent O5.
-    //     */
-    //     clipper::Coord_orth position2;
-    //     /**
-    //     * @brief Second atom of pyranose sugar that would be linked to this amino acid, for ASN-NAG position2 would represent O5.
-    //     */
-    //     clipper::Coord_orth position3;
-    //
-    //
-    //     float bond_length = 1.45;
-    // };
-    //
-    // /**
-    //  * @class Asparagine
-    //  * @brief A class representing the ASN amino acid
-    //  * Inherits from SailsAminoAcid class.
-    //  */
-    // class Asparagine : public AminoAcid {
-    // public:
-    //     /**
-    //      * @brief Constructor for ASN class.
-    //      * @param aa The reference to the clipper::MMonomer object.
-    //      * @details This constructor initializes a new ASN object by calling the base class constructor SailsAminoAcid.
-    //      * It sets the atom_1, atom_2, atom_3, angle, and torsion parameters to "CB", "CG", "ND2", 110, and 180 respectively.
-    //      */
-    //     explicit Asparagine(clipper::MMonomer&aa): AminoAcid(aa, "CB", "CG", "ND2", 105, 120, -86, 180) {
-    //     };
-    //
-    //     /**
-    //      * @brief Get the value of position1.
-    //      * @see SailsAminoAcid
-    //      * @return The position1 value of type Coord_orth.
-    //      */
-    //     clipper::Coord_orth get_position1() const {
-    //         return position1;
-    //     }
-    //
-    //     /**
-    //      * @brief Get the value of position2.
-    //      * @see SailsAminoAcid
-    //      * @return The position2 coordinate of the Coord_orth.
-    //      */
-    //     clipper::Coord_orth get_position2() const {
-    //         return position2;
-    //     }
-    //
-    //     /**
-    //     * @brief Get the value of position3.
-    //     * @see SailsAminoAcid
-    //     * @return The position2 coordinate of the Coord_orth.
-    //     */
-    //     clipper::Coord_orth get_position3() const {
-    //         return position3;
-    //     }
-    // };
-    //
-    // /**
-    //  * @class Sugar
-    //  * @brief A class representing a sugar molecule in a Sails model.
-    //  */
-    // class Sugar {
-    // public:
-    //     /**
-    //      * @brief Constructs a Sugar object.
-    //      *
-    //      * @param sugar A reference to a MMonomer object representing the sugar molecule.
-    //      * @param atom_1 A std::string representing the name of the first atom.
-    //      * @param atom_2 A std::string representing the name of the second atom.
-    //      * @param target_1 A Coord_orth object representing the target coordinate of the first atom.
-    //      * @param target_2 A Coord_orth object representing the target coordinate of the second atom.
-    //      *
-    //      * @return None
-    //      */
-    //     explicit Sugar(clipper::MMonomer&sugar,
-    //                    const std::string&atom_1,
-    //                    const std::string&atom_2,
-    //                    const std::string&atom_3,
-    //                    clipper::Coord_orth target_1,
-    //                    clipper::Coord_orth target_2,
-    //                    clipper::Coord_orth target_3);
-    //
-    //     float score_sugar(const clipper::Xmap<float>&xmap);
-    //
-    // protected:
-    //     /**
-    //      * @brief The transformed sugar into the position defined my target1 and target2
-    //      */
-    //     clipper::MMonomer positioned_monomer;
-    // };
-    //
-    // /**
-    //  * @class NAG
-    //  * @brief Represents a NAG (N-Acetylglucosamine) molecule.
-    //  *
-    //  * This class inherits from SailsSugar and provides a specialized implementation
-    //  * for NAG molecules. It includes methods to get the positioned monomer.
-    //  */
-    // class NAG : public Sugar {
-    // public:
-    //     /**
-    //      * @brief Constructs a NAG object using a sugar monomer, and two orthogonal coordinates positions
-    //      * @param sugar The sugar monomer object
-    //      * @param pos1 The first orthogonal coordinate position
-    //      * @param pos2 The second orthogonal coordinate position
-    //      */
-    //     explicit NAG(clipper::MMonomer&sugar, clipper::Coord_orth&pos1, clipper::Coord_orth&pos2,
-    //                  clipper::Coord_orth&pos3): Sugar(sugar, "C1", "O5", "C5", pos1, pos2, pos3) {
-    //     }
-    //
-    //     /**
-    //      * @brief Retrieves the positioned monomer.
-    //      *
-    //      * This method returns the positioned monomer object of the SailsSugar class.
-    //      *
-    //      * @return The positioned monomer object.
-    //      */
-    //     clipper::MMonomer get_positioned_monomer() {
-    //         return positioned_monomer;
-    //     }
-    // };
+
+    enum ResidueType {
+        ASN, NAG
+    };
+
+    static std::map<std::string, ResidueType> ResidueMap = {
+            {"ASN", ResidueType::ASN},
+            {"NAG", ResidueType::NAG}
+    };
+
 }
 #endif //SAILS_MODEL_H
