eee5fee3dfd587c3ef38205f092a306fed8bfdfe
Jordan Dialpuri <44945647+Dialpuri@users.noreply.github.com>
2024-02-17 16:21:23 +0000

    Add new test cases to test_sails_lib.py
    Several unit test cases have been added to test_sails_lib.py. The new cases are designed to test specific functions such as constructor testing for the Output class and existence checks for "Input", "Output", and "

--- tests/test_sails_lib.py
+++ tests/test_sails_lib.py
@@ -1,4 +1,18 @@
 import pysails as sails
+import pytest
 def test_sails_input_exists():
     assert "Input" in dir(sails)
 
+def test_sails_output_exists():
+    assert "Output" in dir(sails)
+
+def test_sails_find_exists():
+    assert "Find" in dir(sails)
+
+def test_sails_input_constructors():
+    with pytest.raises(TypeError):
+        i = sails.Input("", "", "", "", "", "", "", "", "")
+    i = sails.Input("", "", "", "", "", "", "", 0, "")
+
+def test_sails_output_constructors():
+    o = sails.Output("")
