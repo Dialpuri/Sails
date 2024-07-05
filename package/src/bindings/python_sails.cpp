2f55e543e6e8f911ce83f51610df9d453c93c493
Dialpuri <dialpuri.jordan@gmail.com>
2024-07-05 14:19:42 +0100

    Add 'run' function to python_sails bindings
    Added a 'run' function to the python bindings for the Sails library in python_sails.cpp. This function initializes a Sails::ResidueDatabase and a Sails::JSONLoader, and is exposed to the Python script through 'm.def'. Included additional headers to support these changes.

--- package/src/bindings/python_sails.cpp
+++ package/src/bindings/python_sails.cpp
@@ -4,12 +4,18 @@
 #include <nanobind/nanobind.h>
 #include <nanobind/stl/string.h>
 
+#include "../include/sails-json.h"
+#include "../include/sails-model.h"
 
 namespace nb = nanobind;
-
 using namespace nb::literals;
 
+void run() {
+    Sails::ResidueDatabase database;
+    Sails::JSONLoader loader = {"/home/jordan/dev/Sails/package/data/data.json", database};
+
+}
 
 NB_MODULE(sails_module, m) {
-
+m.def("run", &run);
 }
