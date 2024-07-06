3bf15be3f24516d6a27b9d14d4265cb14ac0ca6b
Jordan Dialpuri <44945647+Dialpuri@users.noreply.github.com>
2024-07-06 23:15:21 +0100

    Update Python bindings to process glycans
    This commit updates the Python bindings to include functionality for processing glycans within the Sails module. New includes have been added for sails-sequence, sails-glycan, and sails-topology. The main function now reads a structure file, finds N-glycosylation sites, and generates a glycan tree structure.

--- package/src/bindings/python_sails.cpp
+++ package/src/bindings/python_sails.cpp
@@ -2,20 +2,58 @@
 // Created by Jordan Dialpuri on 16/02/2024.
 //
 #include <nanobind/nanobind.h>
-#include <nanobind/stl/string.h>
 
 #include "../include/sails-json.h"
-#include "../include/sails-model.h"
+#include "../include/sails-sequence.h"
+#include "../include/sails-glycan.h"
+#include "../include/sails-topology.h"
+
+#include "gemmi/model.hpp" // for Structure
+#include "gemmi/mmread.hpp" // for read_structure
+#include "gemmi/resinfo.hpp" // for find_tabulated_residue
 
 namespace nb = nanobind;
 using namespace nb::literals;
 
+
 void run() {
     Sails::ResidueDatabase database;
-    Sails::JSONLoader loader = {"/home/jordan/dev/Sails/package/data/data.json", database};
+    Sails::JSONLoader loader = {"package/data/data.json", database};
 
+    const std::string path = "package/models/5fji/5fji.cif";
+    gemmi::Structure structure = gemmi::read_structure_file(path);
+
+    Sails::Glycosites glycosites = Sails::find_n_glycosylation_sites(structure);
+
+    for (const auto& glycosite: glycosites) {
+        auto glycan = Sails::find_glycan_topology(structure, glycosite);
+    }
+
+    std::cout << glycosites.size() << " glycosites found." << std::endl;
+
+    Sails::Sugar sugar1 = {"C1", 101};
+    Sails::Sugar sugar2 = {"C2", 102};
+    Sails::Sugar sugar3 = {"C3", 103};
+    Sails::Sugar sugar4 = {"C4", 104};
+
+    Sails::Glycan glycan;
+    glycan.addLinkage(sugar1, sugar2);
+    glycan.addLinkage(sugar2, sugar3);
+    glycan.addLinkage(sugar1, sugar4);
+
+
+
+//    glycan.print_list();
+
+    // generate tree structure of glycans
+    // check glycosite residues
+    // check donor atoms
+    // check for attached sugar with neighbour search
+    // check donors of attached sugar for other sugars
+    // tree structure
+    //
 }
 
 NB_MODULE(sails_module, m) {
-m.def("run", &run);
+    m.def("run", &run);
 }
