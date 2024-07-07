ddc5fd35578314e418c41cd3642f190b6a800c56
Jordan Dialpuri <44945647+Dialpuri@users.noreply.github.com>
2024-07-07 12:42:42 +0100

    Add utility functions for handling glycosites in Sails::Utils
    Introduce `format_residue_key`, `get_chain_from_glycosite`, `get_residue_from_glycosite`, and `get_atom_from_glycosite` for improved glycosite management. These functions facilitate easier

--- package/src/include/sails-utils.h
+++ package/src/include/sails-utils.h
@@ -6,17 +6,32 @@
 #define SAILS_SAILS_UTILS_H
 
 #include <iostream>
-#include <filesystem>
 
 #include "gemmi/math.hpp"
+#include "gemmi/model.hpp"
+
+#include "sails-model.h"
 
 namespace Sails::Utils {
-    inline bool check_file_exists(const std::string& path) {
-        return std::filesystem::exists(path);
-    }
-
     inline void print_vector(const gemmi::Vec3& vec) {
         std::cout << "(" << vec.x << ", " << vec.y << ", " << vec.z << ")" << std::endl;
+    }
+
+    inline std::string format_residue_key(const gemmi::Residue* residue) {
+        return residue->name + "-" + residue->seqid.str();
+    }
+
+    gemmi::Chain get_chain_from_glycosite(const Glycosite& site, const gemmi::Structure& structure) {
+        return structure.models[site.model_idx].chains[site.chain_idx];
+    }
+
+    gemmi::Residue get_residue_from_glycosite(const Glycosite& site,const gemmi::Structure& structure) {
+        return structure.models[site.model_idx].chains[site.chain_idx].residues[site.residue_idx];
+    }
+
+    gemmi::Atom get_atom_from_glycosite(const Glycosite& site, const gemmi::Structure& structure) {
+        if (site.atom_idx == -1) {throw std::runtime_error("Site has not been initialised from a Mark");}
+        return structure.models[site.model_idx].chains[site.chain_idx].residues[site.residue_idx].atoms[site.atom_idx];
     }
 
 } // namespace Sails::Utils
