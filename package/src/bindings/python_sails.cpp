5a48f2226ad68258febd9f1fd6f9c3cc00602db8
Jordan Dialpuri <44945647+Dialpuri@users.noreply.github.com>
2024-07-08 18:04:49 +0100

    Refactor Python bindings and main Sails structure
    This commit moves significant portions of the python_sails.cpp code to sails.cpp. The logic for handling Sails object creation, calculations, and output writing that was originally in the Python bindings file has been transferred to sails.cpp. The Python bindings now only call the relevant functions defined within sails.cpp

--- package/src/bindings/python_sails.cpp
+++ package/src/bindings/python_sails.cpp
@@ -2,49 +2,10 @@
 // Created by Jordan Dialpuri on 16/02/2024.
 //
 #include <nanobind/nanobind.h>
-
-#include "../include/sails-json.h"
-#include "../include/sails-sequence.h"
-#include "../include/sails-glycan.h"
-#include "../include/sails-topology.h"
-
-#include "gemmi/model.hpp" // for Structure
-#include "gemmi/mmread.hpp" // for read_structure
-#include "gemmi/resinfo.hpp" // for find_tabulated_residue
+#include "../cpp/sails.cpp"
 
 namespace nb = nanobind;
 using namespace nb::literals;
-
-
-void run() {
-    Sails::ResidueDatabase database;
-    Sails::JSONLoader loader = {"package/data/data.json", database};
-
-    const std::string path = "package/models/5fji/5fji.cif";
-    gemmi::Structure structure = gemmi::read_structure_file(path);
-
-    Sails::Glycosites glycosites = Sails::find_n_glycosylation_sites(structure);
-    std::string dot_file;
-
-    for (auto& glycosite: glycosites) {
-        auto glycan = Sails::find_glycan_topology(structure, glycosite, database);
-
-        if (glycan.has_value()) {
-            dot_file += glycan->get_dot_string(structure, database);
-        }
-    }
-    std::ofstream of("package/graphs/all-glycans.dot");
-    of << dot_file << std::endl;
-    of.close();
-
-    // generate tree structure of glycans
-    // check glycosite residues
-    // check donor atoms
-    // check for attached sugar with neighbour search
-    // check donors of attached sugar for other sugars
-    // tree structure
-    //
-}
 
 NB_MODULE(sails_module, m) {
     m.def("run", &run);
--- package/src/cpp/sails.cpp
+++ package/src/cpp/sails.cpp
@@ -2,9 +2,44 @@
 // Created by Jordan Dialpuri on 06/07/2024.
 //
 
-#include <iostream>
+#include "../include/sails-json.h"
+#include "../include/sails-sequence.h"
+#include "../include/sails-glycan.h"
+#include "../include/sails-topology.h"
+#include "../include/sails-linkage.h"
 
+#include "gemmi/model.hpp" // for Structure
+#include "gemmi/mmread.hpp" // for read_structure
+#include "gemmi/resinfo.hpp" // for find_tabulated_residue
+
+
+void run() {
+    Sails::JSONLoader loader = {"package/data/data.json"};
+    Sails::ResidueDatabase residue_database = loader.load_residue_database();
+    Sails::LinkageDatabase linkage_database = loader.load_linkage_database();
+
+    const std::string path = "package/models/5fji/5fji.cif";
+    gemmi::Structure structure = gemmi::read_structure_file(path);
+
+    Sails::Glycosites glycosites = Sails::find_n_glycosylation_sites(structure);
+    Sails::Topology topology = {structure, residue_database};
+
+    Sails::Model model = {structure, linkage_database,residue_database};
+    for (auto& glycosite: glycosites) {
+        auto glycan = topology.find_glycan_topology(glycosite);
+        if (!glycan.has_value()) {continue;}
+
+        // remove sugars with a lot of red difference density nearby -> indicates its wrong and then store those so we dont try rebuilding there...
+
+        // find terminal sugars
+        auto residue = Sails::Utils::get_residue_from_glycosite(glycosite, structure);
+
+        auto new_glycan = model.extend(glycan.value(), residue.seqid.num.value);
+
+    }
+
+}
 
 int main() {
-    std::cout << "Reached main" << std::endl;
+    run();
 }
