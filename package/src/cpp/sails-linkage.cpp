f87be810ecf2f35e5a09a14fdc04d2b70b91684b
Jordan Dialpuri <44945647+Dialpuri@users.noreply.github.com>
2024-07-14 11:58:48 +0100

    Enhance glycan extension with scoring and refinement
    This update modifies the glycan extension method in the Model class to score residues into experimental data. Affected features include atomic position calculation, atom superposition, and residue addition. The improvements allow optional resort to real space simplex refinement of residues, based on a provided boolean flag. Use of cached monomers is also introduced to optimize performance.

--- package/src/cpp/sails-linkage.cpp
+++ package/src/cpp/sails-linkage.cpp
@@ -3,44 +3,99 @@
 //
 
 #include "../include/sails-linkage.h"
+#include "../include/sails-refine.h"
 
-#include <gemmi/mmread.hpp>
-#include <gemmi/modify.hpp>
-#include <gemmi/qcp.hpp>
-#include <gemmi/to_pdb.hpp>
-#include <src/include/sails-topology.h>
 
-Sails::Glycan Sails::Model::extend(Glycan &glycan, int base_seqid) {
-    std::vector<Sugar *> terminal_sugars = glycan.get_terminal_sugars(base_seqid);
+bool Sails::Model::check_entry_in_database(gemmi::Residue residue) {
+    if (linkage_database.find(residue.name) == linkage_database.end()) {
+        std::cout << "Residue type: " << residue.name << " is not in Sails' Linkage Database" << std::endl;
+        return true;
+    }
+    return false;
+}
+
+Sails::Glycan Sails::Model::extend(Glycan &glycan, int base_seqid, Density &density) {
+    const std::vector<Sugar *> terminal_sugars = glycan.get_terminal_sugars(base_seqid);
 
     for (auto &terminal_sugar: terminal_sugars) {
         auto residue = Utils::get_residue_from_glycosite(terminal_sugar->site, structure);
-        if (linkage_database.find(residue.name) == linkage_database.end()) {
-            // std::cout << "Residue type: " << residue.name << " is not in Sails' Linkage Database" << std::endl;
-            continue;
-        }
+        // std::cout << "Terminal sugar is " << Utils::format_residue_key(&residue) << std::endl;
+        if (check_entry_in_database(residue)) continue;
 
         for (auto &data: linkage_database[residue.name]) {
-            gemmi::Residue new_sugar = calculate_residue_translation(residue, data);
-            int new_seqid = terminal_sugar->seqId+100;
-            new_sugar.seqid = gemmi::SeqId(new_seqid, 0);
-            auto all_residues = &structure.models[terminal_sugar->site.model_idx].chains[terminal_sugar->site.chain_idx].residues;
-            all_residues->insert(all_residues->end(),std::move(new_sugar));
+            std::optional<SuperpositionResult> opt_result = add_residue(residue, data, density, true);
+            if (!opt_result.has_value()) { continue; };
+
+            SuperpositionResult result = opt_result.value();
+            result.new_residue.seqid = gemmi::SeqId(terminal_sugar->seqId + 100, 0);
+
+            auto all_residues = &structure.models[terminal_sugar->site.model_idx].chains[terminal_sugar->site.chain_idx]
+                    .residues;
+
+            all_residues->insert(all_residues->end(), std::move(result.new_residue));
         }
     }
 
     Topology topology = {structure, residue_database};
     return topology.find_glycan_topology(glycan.glycosite);
+
 }
 
-gemmi::Residue Sails::Model::calculate_residue_translation(const gemmi::Residue &residue, LinkageData &data) {
+void Sails::Model::move_acceptor_atomic_positions(std::vector<gemmi::Atom *> &atoms, double length,
+                                                  std::vector<double> &angles, std::vector<double> &torsions) {
+    for (int i = 0; i < 3; i++) {
+        gemmi::Position atom1 = atoms[i]->pos;
+        gemmi::Position atom2 = atoms[i + 1]->pos;
+        gemmi::Position atom3 = atoms[i + 2]->pos;
+        gemmi::Vec3 new_position = calculate_projected_point(atom1, atom2, atom3, length, angles[i], torsions[i]);
+        atoms[i + 3]->pos = gemmi::Position(new_position);
+    }
+}
+
+gemmi::Transform Sails::Model::superpose_atoms(std::vector<gemmi::Atom *> &atoms,
+                                               std::vector<gemmi::Atom> &reference_atoms, double length,
+                                               std::vector<double> &angles, std::vector<double> &torsions) {
+    move_acceptor_atomic_positions(atoms, length, angles, torsions);
+
+    std::vector<gemmi::Position> reference_positions;
+    std::vector<gemmi::Position> new_positions;
+    std::for_each(atoms.begin() + 3, atoms.end(), [&](const gemmi::Atom *a) { new_positions.emplace_back(a->pos); });
+    std::for_each(reference_atoms.begin(), reference_atoms.end(), [&](const gemmi::Atom &a) {
+        reference_positions.emplace_back(a.pos);
+    });
+
+    return calculate_superposition(reference_positions, new_positions);
+}
+
+void Sails::Model::save_pdb(const std::string &path) {
+    std::ofstream of;
+    of.open(path);
+    write_pdb(structure, of);
+    of.close();
+}
+
+void Sails::Model::remove_leaving_atom(Sails::LinkageData &data, gemmi::Residue& reference_library_monomer, gemmi::Residue& new_monomer) {
+    std::string leaving_atom = "O" + std::to_string(data.acceptor_number); // Ox atom leaves in sugars
+    auto leaving_atom_iter = std::remove_if(new_monomer.atoms.begin(), new_monomer.atoms.end(),
+                                            [&leaving_atom](const gemmi::Atom &a) { return a.name == leaving_atom; });
+    new_monomer.atoms.erase(leaving_atom_iter, new_monomer.atoms.end());
+
+    leaving_atom_iter = std::remove_if(reference_library_monomer.atoms.begin(), reference_library_monomer.atoms.end(),
+                                       [&leaving_atom](const gemmi::Atom &a) { return a.name == leaving_atom; });
+    reference_library_monomer.atoms.erase(leaving_atom_iter, reference_library_monomer.atoms.end());
+}
+
+std::optional<Sails::SuperpositionResult> Sails::Model::add_residue(
+    const gemmi::Residue &residue, LinkageData &data, Density &density, bool refine) {
     std::vector<gemmi::Atom *> atoms;
 
     // find library monomer for acceptor residue
     auto library_monomer = get_monomer(data.acceptor);
 
     if (!library_monomer.has_value()) { throw std::runtime_error("Could not get monomer"); }
-    auto reference_library_monomer = library_monomer.value();
+    auto reference_library_monomer = gemmi::Residue(library_monomer.value());
+    auto new_monomer = gemmi::Residue(library_monomer.value());
+
     std::vector<gemmi::Atom> reference_atoms;
 
     ResidueData donor_residue = residue_database[data.donor];
@@ -65,41 +120,47 @@ gemmi::Residue Sails::Model::calculate_residue_translation(const gemmi::Residue 
 
     if (atoms.size() != 6) { throw std::runtime_error("Unexpected atom count"); }
 
-    TorsionSet linkage_torsions = data.torsions;
-    std::vector<double> mean_torsions = data.torsions.get_means_in_order();
+    std::vector<double> torsions = data.torsions.get_means_in_order();
     std::vector<double> angles = data.angles.get_in_order();
+    double length = data.length;
 
-    for (int i = 0; i < 3; i++) {
-        gemmi::Position atom1 = atoms[i]->pos;
-        gemmi::Position atom2 = atoms[i + 1]->pos;
-        gemmi::Position atom3 = atoms[i + 2]->pos;
+    gemmi::Transform superpose_result = superpose_atoms(atoms, reference_atoms, length, angles, torsions);
+    gemmi::transform_pos_and_adp(new_monomer, superpose_result);
 
-        gemmi::Vec3 new_position = calculate_projected_point(atom1, atom2, atom3, 2.0, angles[i], mean_torsions[i]);
-        atoms[i + 3]->pos = gemmi::Position(new_position);
+    // remove leaving atom
+    remove_leaving_atom(data, reference_library_monomer, new_monomer);
+
+    SuperpositionResult result = {new_monomer, superpose_result, reference_library_monomer};
+
+    float rscc = density.rscc_score(result);
+    if (rscc < 0.3) {
+        return std::nullopt;
     }
 
-    std::vector<gemmi::Position> reference_positions;
-    std::vector<gemmi::Position> new_positions;
-
-    std::for_each(atoms.begin() + 3, atoms.end(), [&](const gemmi::Atom *a) { new_positions.emplace_back(a->pos); });
-
-    std::for_each(reference_atoms.begin(), reference_atoms.end(), [&](const gemmi::Atom a) {
-        reference_positions.emplace_back(a.pos);
-    });
-
-    auto superpose_result = calculate_superposition(reference_positions, new_positions);
-    transform_pos_and_adp(reference_library_monomer, superpose_result); // gemmi function
-
-    return reference_library_monomer;
+    if (refine) {
+        TorsionAngleRefiner refiner = {atoms, reference_atoms, density, result, length};
+        SuperpositionResult final_result = refiner.refine(angles, torsions);
+        return final_result;
+    }
+    return result;
 }
 
+
 std::optional<gemmi::Residue> Sails::Model::get_monomer(const std::string &monomer) {
+    // lookup mononer in cache
+    if (monomers.find(monomer) != monomers.end()) {
+        return monomers[monomer];
+    }
+
     std::string path = monomer_library_path + "/" + char(std::tolower(monomer.front())) + "/" + monomer + ".cif";
     if (!std::filesystem::exists(path)) {
         std::cout << path << " monomer does not exist" << std::endl;
         return std::nullopt;
     }
     gemmi::Structure structure = gemmi::read_structure_file(path, gemmi::CoorFormat::Detect);
-    return structure.models[0].chains[0].residues[0];
+    gemmi::Residue residue = structure.models[0].chains[0].residues[0];
+
+    monomers[monomer] = residue;
+    return residue;
 }
 
--- package/src/include/sails-linkage.h
+++ package/src/include/sails-linkage.h
@@ -7,12 +7,35 @@
 
 #include "sails-glycan.h"
 #include "sails-utils.h"
+#include "sails-topology.h"
 #include "sails-vector.h"
-#include <gemmi/cif.hpp>             // for cif::read_file
+#include "sails-density.h"
+
 #include <string>
 
+#include <gemmi/cif.hpp>             // for cif::read_file
+#include <gemmi/modify.hpp>
+#include <gemmi/mmread.hpp>
+#include <gemmi/to_pdb.hpp>
 
 namespace Sails {
+
+
+    struct SuperpositionResult {
+        SuperpositionResult() = default;
+        SuperpositionResult(gemmi::Residue new_residue, const gemmi::Transform &transformation, gemmi::Residue reference_residue)
+            : new_residue(std::move(new_residue)),
+              transformation(transformation),
+              reference_residue(std::move(reference_residue)) {
+        }
+
+        bool is_null() const {return transformation.vec.has_nan();}
+
+        gemmi::Residue new_residue;
+        gemmi::Transform transformation;
+        gemmi::Residue reference_residue;
+    };
+
     struct Model {
         Model() = default;
         Model(gemmi::Structure &structure, LinkageDatabase &linkage_database, ResidueDatabase &residue_database)
@@ -20,14 +43,28 @@ namespace Sails {
             monomer_library_path = Utils::get_environment_variable("CLIBD") + "/monomers";
         }
 
+        bool check_entry_in_database(gemmi::Residue residue);
+
         /**
          * Extends the given glycan by adding new sugars based on the linkage database.
          *
          * @param glycan The glycan to be extended.
          * @param base_seqid The base sequence ID to determine terminal sugars.
+         * @param density The density class used to score residues into experimental data.
          * @return The extended glycan.
          */
-        Glycan extend(Glycan &glycan, int base_seqid);
+        Glycan extend(Glycan &glycan, int base_seqid, Density& density);
+
+        static void move_acceptor_atomic_positions(std::vector<gemmi::Atom *>& atoms, double length,
+                                                   std::vector<double>& angles, std::vector<double>& torsions);
+
+        static gemmi::Transform superpose_atoms(std::vector<gemmi::Atom *>& atoms, std::vector<gemmi::Atom>& reference_atoms, double length, std::vector<double> &angles, std::vector<
+                                                    double> &torsions);
+
+        void save_pdb(const std::string& path);
+
+        void remove_leaving_atom(Sails::LinkageData &data, gemmi::Residue& reference_library_monomer,
+                                 gemmi::Residue& new_monomer);
 
     private:
         /**
@@ -46,16 +83,20 @@ namespace Sails {
          *
          * @param residue The residue to calculate the translation for.
          * @param data The linkage data containing donor and acceptor information.
-         * @return The translated residue.
+         * @param density The constructed density object for accessing experimental data.
+         * @param refine Boolean to run real space simplex refinement on the residue.
+         * @return Optionally, the translated residue.
          * @throws std::runtime_error if any required atom or data is not found, or unexpected atom count.
          */
-        gemmi::Residue calculate_residue_translation(const gemmi::Residue& residue, LinkageData& data);
+        std::optional<Sails::SuperpositionResult> add_residue(
+            const gemmi::Residue &residue, LinkageData &data, Density &density, bool refine);
 
     private:
         gemmi::Structure structure;
         LinkageDatabase linkage_database;
         ResidueDatabase residue_database;
         std::string monomer_library_path;
+        std::map<std::string, gemmi::Residue> monomers;
     };
 
 
