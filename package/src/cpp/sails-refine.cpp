7b813b68c8a64e7e98fa52eab18b4842b96d0cdc
Jordan Dialpuri <44945647+Dialpuri@users.noreply.github.com>
2024-07-18 15:40:05 +0100

    Implement deviation check in sails-refine
    The code has been updated to add a deviation check for both angles and torsions. This will return a score of 100 if the deviation is greater than or equal to 1. Additionally, enhanced the refine function to utilize mean and range values of angles and torsions instead of initial values.

--- package/src/cpp/sails-refine.cpp
+++ package/src/cpp/sails-refine.cpp
@@ -8,23 +8,36 @@ double Sails::TorsionAngleRefiner::score_function(std::vector<double> &all_angle
     std::vector<double> angles = {all_angles[0], all_angles[1], all_angles[2]};
     std::vector<double> torsions = {all_angles[3], all_angles[4], all_angles[5]};
 
+    for (int i = 0; i < 3; i++) {
+        double deviaton = abs(m_angle_mean[i]-angles[i]);
+        if (deviaton >= 1*m_angle_range[i]) {
+            return 100;
+        }
+    }
+    for (int i = 0; i < 3; i++) {
+        double deviaton = abs(m_torsion_mean[i]-torsions[i]);
+        if (deviaton >= 1*m_torsion_range[i]) {
+            return 100;
+        }
+    }
+
+
     gemmi::Residue residue = gemmi::Residue(m_reference_residue);
 
     gemmi::Transform superpose_result = Model::superpose_atoms(m_all_atoms, m_reference_atoms, m_length, angles,
                                                                torsions);
     gemmi::transform_pos_and_adp(residue, superpose_result);
     SuperpositionResult result = {residue, superpose_result, m_reference_residue};
-    // return -m_density.rscc_score(result);
-    return -m_density->atomwise_score(residue);
+
+    float score = -m_density->atomwise_score(residue);
+    return score;
 }
 
-Sails::SuperpositionResult Sails::TorsionAngleRefiner::refine(
-    std::vector<double> &initial_angles,
-    std::vector<double> &initial_torsions) {
+Sails::SuperpositionResult Sails::TorsionAngleRefiner::refine() {
 
     std::vector<double> initial_simplex = {
-        initial_angles[0], initial_angles[1], initial_angles[2],
-        initial_torsions[0], initial_torsions[1], initial_torsions[2]
+        m_angle_mean[0], m_angle_mean[1], m_angle_mean[2],
+        m_torsion_mean[0], m_torsion_mean[1], m_torsion_mean[2]
     };
 
     auto lambda = [&](std::vector<double> &x) -> double {
@@ -47,5 +60,12 @@ Sails::SuperpositionResult Sails::TorsionAngleRefiner::refine(
             Model::superpose_atoms(m_all_atoms, m_reference_atoms, m_length, final_angles, final_torsions);
     gemmi::transform_pos_and_adp(residue, final_result);
     SuperpositionResult result = {residue, final_result, m_reference_residue};
+
+    // std::vector<std::string> labels = {"alpha", "beta", "gamma", "psi", "phi", "omega"};
+    // std::cout << "\nLabel\tOriginal\tNew" << std::endl;
+    // for (int i = 0; i < final_simplex.size(); i++) {
+    //     std::cout << labels[i] << "\t" << initial_simplex[i] << "\t" << final_simplex[i] << std::endl;
+    // }
+
     return result;
 }
--- package/src/include/sails-refine.h
+++ package/src/include/sails-refine.h
@@ -25,20 +25,34 @@ namespace Sails {
             std::vector<gemmi::Atom> &reference_atoms,
             Density& density,
             SuperpositionResult& superposition_result,
-            double length
+            double length,
+            std::vector<double> & angle_mean,
+            std::vector<double> & angle_range,
+            std::vector<double> & torsion_mean,
+            std::vector<double> & torsion_range
+
             ) : m_all_atoms(atoms),
             m_reference_atoms(reference_atoms),
             m_density(&density),
             m_reference_residue(superposition_result.reference_residue),
-            m_length(length){}
+            m_length(length),
+            m_angle_mean(angle_mean),
+            m_torsion_mean(torsion_mean),
+            m_angle_range(angle_range),
+            m_torsion_range(torsion_range) {}
 
 
         double score_function(std::vector<double>& angles); // {alpha, beta, gamma, phi, psi, omega}
 
-        SuperpositionResult refine(std::vector<double> &initial_angles, std::vector<double> &initial_torsions);
+        SuperpositionResult refine();
 
     private:
         double m_length;
+        std::vector<double> m_angle_range;
+        std::vector<double> m_torsion_range;
+        std::vector<double> m_angle_mean;
+        std::vector<double> m_torsion_mean;
+
         std::vector<gemmi::Atom *> m_all_atoms;
         std::vector<gemmi::Atom> m_reference_atoms;
         Density* m_density;
