b35426c52fc9b1a8d93ca2f8b8e7054d4468f79c
Jordan Dialpuri <44945647+Dialpuri@users.noreply.github.com>
2024-07-21 17:45:59 +0100

    Add function to find C-glycosylation sites
    A new function, find_c_glycosylation_sites, has been added to find C-glycosylation sites in a given structure. This function considers the sequence pattern W-X-X-W for identifying the sites. The

--- package/src/cpp/sails-sequence.cpp
+++ package/src/cpp/sails-sequence.cpp
@@ -31,3 +31,29 @@ Sails::Glycosites Sails::find_n_glycosylation_sites(const gemmi::Structure &stru
 
     return glycosites;
 }
+
+Sails::Glycosites Sails::find_c_glycosylation_sites(const gemmi::Structure &structure) {
+    std::vector<Glycosite> glycosites;
+    auto models = structure.children();
+    for (int model_idx = 0; model_idx < models.size(); model_idx++) {
+
+        auto chains = models[model_idx].children();
+        for (int chain_idx = 0; chain_idx < chains.size(); chain_idx++) {
+
+            auto residues = chains[chain_idx].children();
+            for (int residue_idx = 0; residue_idx < residues.size() - 3; residue_idx++) {
+
+                char first = gemmi::find_tabulated_residue(residues[residue_idx].name).one_letter_code;
+                if (first != 'W') {continue;}
+
+                char fourth = gemmi::find_tabulated_residue(residues[residue_idx + 3].name).one_letter_code;
+                if (fourth != 'W') {continue;}
+
+                glycosites.emplace_back(model_idx, chain_idx, residue_idx);
+                glycosites.emplace_back(model_idx, chain_idx, residue_idx+3);
+            }
+        }
+    }
+
+    return glycosites;
+}
--- package/src/include/sails-sequence.h
+++ package/src/include/sails-sequence.h
@@ -13,22 +13,34 @@
 #include "sails-model.h"
 
 namespace Sails {
+    /**
+     * @brief Finds N-glycosylation sites in a given structure.
+     *
+     * This function searches for N-glycosylation sites in a given structure. An N-glycosylation site is defined as a
+     * sequence of three consecutive residues: N-X-S/T, where N is the first residue, S/T is the third residue, and X
+     * can be any residue except proline (P). Only sites that satisfy this sequence pattern are considered as
+     * N-glycosylation sites. The function returns a vector of Glycosite objects that represent the position of each
+     * N-glycosylation site in the structure.
+     *
+     * @param structure The structure in which to search for N-glycosylation sites.
+     * @return A vector of Glycosite objects representing the position of each N-glycosylation site in the structure.
+     *         If no N-glycosylation sites are found, an empty vector is returned.
+     */
+    Glycosites find_n_glycosylation_sites(const gemmi::Structure &structure);
 
-        /**
-         * @brief Finds N-glycosylation sites in a given structure.
-         *
-         * This function searches for N-glycosylation sites in a given structure. An N-glycosylation site is defined as a
-         * sequence of three consecutive residues: N-X-S/T, where N is the first residue, S/T is the third residue, and X
-         * can be any residue except proline (P). Only sites that satisfy this sequence pattern are considered as
-         * N-glycosylation sites. The function returns a vector of Glycosite objects that represent the position of each
-         * N-glycosylation site in the structure.
-         *
-         * @param structure The structure in which to search for N-glycosylation sites.
-         * @return A vector of Glycosite objects representing the position of each N-glycosylation site in the structure.
-         *         If no N-glycosylation sites are found, an empty vector is returned.
-         */
-        Glycosites find_n_glycosylation_sites(const gemmi::Structure &structure);
-
+    /**
+     * @brief Finds C-glycosylation sites in a given structure.
+     *
+     * This function searches for C-glycosylation sites in a given structure. An C-glycosylation site is defined as a
+     * sequence of three consecutive residues: W-X-X-W, where either W can be mannosylated.
+     * Only sites that satisfy this sequence pattern are considered as C-glycosylation sites. The function returns a
+     * vector of Glycosite objects that represent the position of each C-glycosylation site in the structure.
+     *
+     * @param structure The structure in which to search for C-glycosylation sites.
+     * @return A vector of Glycosite objects representing the position of each C-glycosylation site in the structure.
+     *         If no C-glycosylation sites are found, an empty vector is returned.
+     */
+    Glycosites find_c_glycosylation_sites(const gemmi::Structure &structure);
 }
 
 #endif //SAILS_SAILS_SEQUENCE_H
