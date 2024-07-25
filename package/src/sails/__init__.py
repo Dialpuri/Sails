691461ee80822397bc4a457592fcc7a4e1d613ad
Jordan Dialpuri <44945647+Dialpuri@users.noreply.github.com>
2024-07-25 13:13:23 +0100

    Refactor import statements for readability
    Reformatted the import statements in `sails/__init__.py` to place each imported name on a new line, following common Python style guidelines. This change improves code readability and makes future modifications more manageable.

--- package/src/sails/__init__.py
+++ package/src/sails/__init__.py
@@ -1,3 +1,18 @@
-from .sails_module import (run, HKL, Reflection, Pair, Structure, Model, Chain, Residue, Atom, Position, Element, SeqId,
-                           n_glycosylate_from_objects, Cell, MTZ, GlycoSite, Dot)
+from .sails_module import (
+                           HKL,
+                           Reflection,
+                           Pair,
+                           Structure,
+                           Model,
+                           Chain,
+                           Residue,
+                           Atom,
+                           Position,
+                           Element,
+                           SeqId,
+                           n_glycosylate_from_objects,
+                           Cell,
+                           MTZ,
+                           GlycoSite,
+                           Dot)
 from .__version__ import __version__
