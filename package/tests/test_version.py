52a118d776fe5aa44f84350d45352fcdef013c79
Jordan Dialpuri <44945647+Dialpuri@users.noreply.github.com>
2024-08-01 20:39:44 +0100

    Add test for version compliance in sails package
    Introduced a new test script to verify the sails package version against a specific regex pattern. Ensures that the version matches the format `v0.0.0` and corresponds to the `__version__` attribute of the package.

---
+++ package/tests/test_version.py
@@ -0,0 +1,21 @@
+import argparse
+import sails
+import re
+
+
+def main(args):
+    pattern = r'v(\d+\.\d+\.\d+)'
+    match = re.search(pattern, args.version)
+    version = args.version
+    if match:
+        version = match.group(1)
+    else:
+        raise RuntimeError(f'Version {args.version} does not match pattern v0.0.0 - Name')
+    assert sails.__version__ == version
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-version', required=True)
+    args = parser.parse_args()
+    main(args)
