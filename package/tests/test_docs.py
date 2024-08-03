da02f29b461477a4309a0f268ed78ebf5b6e5a63
Jordan Dialpuri <44945647+Dialpuri@users.noreply.github.com>
2024-08-03 10:57:52 +0100

    Add test for document version consistency
    Create a test to ensure the version in writerside.cfg matches the sails package version. This includes parsing the XML configuration and comparing the version number.

---
+++ package/tests/test_docs.py
@@ -0,0 +1,16 @@
+import xml.etree.ElementTree as ET
+from pathlib import Path
+import sails
+
+
+def test_doc_version():
+    current_file = Path(__file__)
+    base_dir = current_file.parents[2]
+    doc_path = base_dir / "docs" / "Writerside" / "writerside.cfg"
+
+    assert doc_path.exists()
+    xml = ET.parse(str(doc_path))
+    root = xml.getroot()
+    x = root.find('instance')
+    doc_version = x.attrib['version']
+    assert sails.__version__ == doc_version
