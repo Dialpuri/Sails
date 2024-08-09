57093e55b51a924e2eb36bb3080ed132758f5533
Dialpuri <dialpuri.jordan@gmail.com>
2024-08-09 12:23:15 +0100

    Refactor glycosylation type handling
    Introduce an enum to manage glycosylation types and map respective functions for n_glycosylate, c_glycosylate, and o_mannosylate. Simplify argparse usage by replacing specific boolean options with a type selection.

--- package/src/sails/__init__.py
+++ package/src/sails/__init__.py
@@ -1,6 +1,6 @@
 from .sails_module import (HKL, Reflection, Pair, Structure, Model, Chain, Residue, Atom, Position, Element, SeqId,
-                           n_glycosylate_from_objects, c_glycosylate_from_objects, Cell, MTZ, GlycoSite, Dot, test_snfg,
-                           get_snfg, get_all_snfgs)
+                           n_glycosylate_from_objects, c_glycosylate_from_objects, o_mannosylate_from_objects,
+                           Cell, MTZ, GlycoSite, Dot, test_snfg, get_snfg, get_all_snfgs)
 from .__version__ import __version__
 from .glycosylate import glycosylate
 from .interface import extract_gemmi_mtz, extract_sails_mtz, extract_gemmi_structure, extract_sails_structure, \
