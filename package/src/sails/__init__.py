df20581d14cb31fa81fdd9edbccc05ea36b25fb9
Dialpuri <dialpuri.jordan@gmail.com>
2024-08-14 12:32:11 +0100

    Add EM mode glycosylation and refactor CLI
    Introduces a new `glycosylate_em` function for processing EM data. Refactors the CLI to support both X-ray and EM modes, and updates the argument parsing to accommodate these changes.

--- package/src/sails/__init__.py
+++ package/src/sails/__init__.py
@@ -1,7 +1,7 @@
 from .sails_module import (HKL, Reflection, Pair, Structure, Model, Chain, Residue, Atom, Position, Element, SeqId,
                            n_glycosylate_from_objects, c_glycosylate_from_objects, o_mannosylate_from_objects,
-                           Cell, MTZ, GlycoSite, Dot, test_snfg, get_snfg, get_all_snfgs)
+                           Cell, MTZ, GlycoSite, Dot, test_snfg, get_snfg, get_all_snfgs, Grid, GridData)
 from .__version__ import __version__
-from .glycosylate import glycosylate, Type
+from .glycosylate import glycosylate_xtal, glycosylate_em, Type
 from .interface import extract_gemmi_mtz, extract_sails_mtz, extract_gemmi_structure, extract_sails_structure, \
-    get_sails_structure, get_sails_mtz
+    get_sails_structure, get_sails_mtz, get_sails_map
