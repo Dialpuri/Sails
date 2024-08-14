51b4e64486e4d634a468035b6911b1fc9e746fa5
Dialpuri <dialpuri.jordan@gmail.com>
2024-08-14 14:17:29 +0100

    Simplify glycosylation function imports.
    Updated the import statements in __init__.py to use simplified glycosylation function names. Removed redundant '_from_objects' suffix to improve code readability and consistency. Also, added the new 'AxisOrder' import.

--- package/src/sails/__init__.py
+++ package/src/sails/__init__.py
@@ -1,6 +1,6 @@
 from .sails_module import (HKL, Reflection, Pair, Structure, Model, Chain, Residue, Atom, Position, Element, SeqId,
-                           n_glycosylate_from_objects, c_glycosylate_from_objects, o_mannosylate_from_objects,
-                           Cell, MTZ, GlycoSite, Dot, test_snfg, get_snfg, get_all_snfgs, Grid, GridData)
+                           n_glycosylate, c_glycosylate, o_mannosylate,
+                           Cell, MTZ, GlycoSite, Dot, test_snfg, get_snfg, get_all_snfgs, Grid, AxisOrder)
 from .__version__ import __version__
 from .glycosylate import glycosylate_xtal, glycosylate_em, Type
 from .interface import extract_gemmi_mtz, extract_sails_mtz, extract_gemmi_structure, extract_sails_structure, \
