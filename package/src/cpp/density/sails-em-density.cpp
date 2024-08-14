d2afec16ab92c92fa72ce2a274c1091e4838357f
Dialpuri <dialpuri.jordan@gmail.com>
2024-08-14 14:16:14 +0100

    Add EMDensity class for enhanced density map handling
    Introduced the EMDensity class in the density module to manage and process various density maps. This includes functionalities for retrieving MTZ objects, grid maps, and calculated maps, as well as initializing and updating map details in the constructor.

--- package/src/cpp/density/sails-em-density.cpp
+++ package/src/cpp/density/sails-em-density.cpp
@@ -1,3 +1,15 @@
 //
 // Created by Jordan Dialpuri on 13/08/2024.
 //
+
+#include "../../include/density/sails-density.h"
+#include "../../include/density/sails-em-density.h"
+
+Sails::EMDensity::EMDensity(gemmi::Grid<> &grid) {
+    m_grid = grid;
+
+    gemmi::Ccp4<> m;
+    m.grid = grid;
+    m.update_ccp4_header();
+    m.write_ccp4_map("map.map");
+}
---
+++ package/src/include/density/sails-em-density.h
@@ -0,0 +1,54 @@
+//
+// Created by Jordan Dialpuri on 13/08/2024.
+//
+
+#include "sails-density.h"
+
+namespace Sails {
+
+    class EMDensity : public Density {
+    public:
+        explicit EMDensity(gemmi::Grid<>& grid);
+
+        [[nodiscard]] const gemmi::Mtz *get_mtz() const override { return &m_mtz; }
+
+        [[nodiscard]] const gemmi::Grid<> *get_work_grid() const override { return &m_grid; }
+
+        [[nodiscard]] const gemmi::Grid<> *get_best_grid() const override { return &m_grid; }
+
+        [[nodiscard]] const gemmi::Grid<> *get_difference_grid() const override { return &m_grid; }
+
+        [[nodiscard]] const double get_resolution() const override { return 2.0; }
+
+        [[nodiscard]] std::unordered_map <std::string, gemmi::Grid<>> *get_calculated_maps() override {
+            return &calculated_maps;
+        }
+
+    private:
+        /**
+		 * Best map
+		 */
+        gemmi::Grid<> m_grid{};
+
+        /**
+         * Difference map
+         */
+        gemmi::Grid<> m_difference_grid{};
+
+        /**
+         * Po-Pc map
+         */
+        gemmi::Grid<> m_po_pc_grid{};
+
+        /**
+         * MTZ Object
+         */
+        gemmi::Mtz m_mtz;
+
+        /**
+         * Fc maps for residues in standard positions - used for fast RSCC calculations
+         */
+        std::unordered_map <std::string, gemmi::Grid<>> calculated_maps;
+
+    };
+}
