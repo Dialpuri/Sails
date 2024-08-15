b84e5c8d21ca6a6470ce386b812561b1acd12325
Jordan Dialpuri <44945647+Dialpuri@users.noreply.github.com>
2024-08-15 11:33:04 +0100

    Add calculate_density_for_box method and update doc comments
    Introduced calculate_density_for_box to calculate density for a Residue object within a specified Box. Updated the documentation for consistency and clarity, and removed the unutilized Density class.

--- package/src/include/density/sails-density.h
+++ package/src/include/density/sails-density.h
@@ -13,6 +13,8 @@
 #include <gemmi/dencalc.hpp>
 #include <gemmi/fourier.hpp>
 #include <gemmi/it92.hpp>
+#include <gemmi/c4322.hpp>
+
 #include <gemmi/calculate.hpp>
 #include <gemmi/ccp4.hpp>
 #include <gemmi/modify.hpp>
@@ -75,10 +77,24 @@ namespace Sails {
          * density score method specified in the constructor of the gemmi::DensityCalculator.
          *
          * @param residue The gemmi::Residue object for which the density is calculated.
+         * @param box
          *
          * @return The calculated density grid for the specified box.
          */
-        gemmi::Grid<> calculate_density_for_box(gemmi::Residue &residue) const;
+        gemmi::Grid<> calculate_density_for_box(gemmi::Residue &residue, gemmi::Box<gemmi::Position> &box) const;
+
+	    /**
+         * @brief Calculates the density for a gemmi::Residue object.
+         *
+         * This method takes a gemmi::Residue object and calculates the density
+         * using the gemmi::DensityCalculator class. The density calculation is performed using the
+         * density score method specified in the constructor of the gemmi::DensityCalculator.
+         *
+         * @param residue The gemmi::Residue object for which the density is calculated.
+         *
+         * @return The calculated density grid for the specified box.
+         */
+	    gemmi::Grid<> calculate_density_for_grid(gemmi::Residue &residue) const;
 
         /**
          * @brief Calculates the RSCC (Real Space Correlation Coefficient) score for a given residue.
@@ -195,163 +211,6 @@ namespace Sails {
 
     };
 
-//    class Density {
-//	public:
-//		Density() = default;
-//
-//		explicit Density(const std::string &mtz_path);
-//
-//		explicit Density(gemmi::Mtz &mtz);
-//
-//		Density(Density &d) {
-//			this->m_mtz = std::move(d.m_mtz);
-//			this->m_grid = d.m_grid;
-//			this->calculated_maps = d.calculated_maps;
-//		};
-//
-//
-//		// private:
-//
-//		/**
-//		 * @brief Transforms F and Phi values from an MTZ file into a grid map.
-//		 *
-//		 * This method takes F and Phi values from an MTZ file and transforms them into a grid using an FFT.
-//		 *
-//		 * @param mtz The MTZ file containing the F and Phi values.
-//		 * @param f_col The label of the column in the MTZ file containing the F values e.g. FWT.
-//		 * @param phi_col The label of the column in the MTZ file containing the Phi values e.g PHWT.
-//		 * @param normalise Indicates whether to normalize the resulting grid map.
-//		 * @return The real space grid formed after FFT.
-//		 */
-//		static gemmi::Grid<> load_grid(const gemmi::Mtz &mtz, const std::string &f_col,
-//		                               const std::string &phi_col, bool normalise);
-//
-//
-//		/**
-//		 * @brief Initializes the necessary parameters for density calculation.
-//		 *
-//		 * This method initializes the necessary parameters for density calculation, such as resolution, cell, spacegroup, hkl_info, and grid_sampling.
-//		 * These parameters are obtained from the m_mtz object which represents the input MTZ file.
-//		 */
-//		void initialise_hkl();
-//
-//		/**
-//		 * @brief Loads the reflection data from the specified MTZ file and builds HKL data.
-//		 *
-//		 * This method reads the reflection data from the MTZ file specified by 'f' and 'sig_f' and builds
-//		 * HKL (H, K, L) data. It populates the internal structures with the HKL data for further analysis.
-//		 *
-//		 * @param f The path to the MTZ file containing the reflection data.
-//		 * @param sig_f The path to the MTZ file containing the sigma data.
-//		 */
-//		void load_hkl(const std::string &f, const std::string &sig_f);
-//
-//		/**
-//		 * @brief Forms a list of clipper::Atom objects from a gemmi::Structure object.
-//		 *
-//		 * This method takes a gemmi::Structure object and populates a vector of clipper::Atom objects.
-//		 * Each atom in the structure is converted to a clipper::Atom object and added to the list.
-//		 *
-//		 * @param structure The gemmi::Structure object from which to form the list of atoms.
-//		 * @param atoms The vector of clipper::Atom objects to be populated.
-//		 */
-//		static void form_atom_list(const gemmi::Structure &structure, std::vector<clipper::Atom> &atoms);
-//
-//		/**
-//		 * @brief Recalculates the map based on the given structure.
-//		 *
-//		 * This method takes a gemmi::Structure object and recalculates the map with a clipper sigma-a calculation.
-//		 * This function updates the internal mtz, grid and density grid.
-//		 *
-//		 * @param structure The gemmi::Structure object for which the map needs to be recalculated.
-//		 */
-//		void recalculate_map(gemmi::Structure &structure);
-//
-//		/**
-//		* @brief Recalculates the map based on the given structure.
-//		*
-//		* This method takes a gemmi::Structure object and recalculates the p_o - p_c map, where p_o is the
-//		* internal best map and p_c is calculated with the provided structure. This function should be called after
-//		* recalculate_map
-//		* This function updates the internal mtz, grid and density grid.
-//		*
-//		* @param structure The gemmi::Structure object for which the map needs to be recalculated.
-//		*/
-//		void calculate_po_pc_map(gemmi::Structure &structure);
-//
-//		/**
-//		 * @brief Writes the density data to an MTZ file.
-//		 *
-//		 * This method writes the density data to an MTZ file at the specified path.
-//		 *
-//		 * @param path The path of the MTZ file to be written.
-//		 */
-//		void write_mtz(const std::string &path) const;
-//
-//
-//
-//		// private:
-//		/**
-//		 * Best map
-//		 */
-//		gemmi::Grid<> m_grid{};
-//
-//		/**
-//		 * Difference map
-//		 */
-//		gemmi::Grid<> m_difference_grid{};
-//
-//		/**
-//		 * Po-Pc map
-//		 */
-//		gemmi::Grid<> m_po_pc_grid{};
-//
-//		/**
-//		 * MTZ Object
-//		 */
-//		gemmi::Mtz m_mtz;
-//
-//		/**
-//		 * Fc maps for residues in standard positions - used for fast RSCC calculations
-//		 */
-//		std::unordered_map<std::string, gemmi::Grid<> > calculated_maps;
-//
-//		// clipper HKL - initialised in initialise_hkl
-//		/**
-//		 * Clipper Spacegroup used for map recalcualtion - initialised in initialise_hkl
-//		 */
-//		clipper::Spacegroup m_spacegroup;
-//
-//		/**
-//		 * Clipper Resolution used for map recalcualtion - initialised in initialise_hkl
-//		 */
-//		clipper::Resolution m_resolution;
-//
-//		/**
-//		 * Clipper Cell used for map recalcualtion - initialised in initialise_hkl
-//		 */
-//		clipper::Cell m_cell;
-//
-//		/**
-//		 * Clipper container for reflection metadata used for map recalcualtion
-//		 */
-//		clipper::HKL_info m_hkl_info;
-//
-//		/**
-//		 * Clipper Grid sampling used for map recalcualtion - initialised in initialise_hkl
-//		 */
-//		clipper::Grid_sampling m_grid_sampling;
-//
-//		/**
-//		 * Clipper list of F obs reflections
-//		 */
-//		clipper::HKL_data<clipper::data32::F_sigF> m_fobs; // initialsised in load_hkl
-//
-//		/**
-//		 * Clipper best map
-//		 */
-//		clipper::Xmap<float> m_best_map;
-//	};
 } // namespace Sails
 
 #endif //SAILS_SAILS_DENSITY_H
