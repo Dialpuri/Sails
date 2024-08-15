33957198af9aced87ee21c5aefa214747bb563cf
Jordan Dialpuri <44945647+Dialpuri@users.noreply.github.com>
2024-08-15 11:32:54 +0100

    Remove redundant CCP4 map writing code
    Eliminate unused CCP4 map writing operations within the EMDensity constructor to streamline the code and improve maintainability. The function now focuses solely on initializing the grid.

--- package/src/cpp/density/sails-em-density.cpp
+++ package/src/cpp/density/sails-em-density.cpp
@@ -7,9 +7,4 @@
 
 Sails::EMDensity::EMDensity(gemmi::Grid<> &grid) {
     m_grid = grid;
-
-    gemmi::Ccp4<> m;
-    m.grid = grid;
-    m.update_ccp4_header();
-    m.write_ccp4_map("map.map");
 }
