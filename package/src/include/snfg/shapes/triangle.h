7cb5a16cc9507a50adc48976e2d4644e6399bc6d
Jordan Dialpuri <44945647+Dialpuri@users.noreply.github.com>
2024-09-16 13:04:27 +0100

    Add Triangle SNFG shape

---
+++ package/src/include/snfg/shapes/triangle.h
@@ -0,0 +1,34 @@
+//
+// Created by Jordan Dialpuri on 07/08/2024.
+//
+
+#ifndef TRIANGLE_H
+#define TRIANGLE_H
+
+#include "../sails-snfg-shape.h"
+
+namespace Sails {
+    struct Triangle : SNFGShapeBase {
+        explicit Triangle(const SNFGNode *node);
+
+    protected:
+        [[nodiscard]] KwargList get_kwargs() const override { return {kwargs}; };
+
+        [[nodiscard]] std::vector<std::string> get_type() const override { return {type}; }
+
+        [[nodiscard]] int get_priority() const override { return priority; }
+
+        [[nodiscard]] std::string get_special_tags() const override { return ""; }; // no special tags for triangle
+
+        [[nodiscard]] std::string get_tooltips() const override { return tooltip;}
+
+    private:
+        std::map<std::string, std::string> kwargs = {};
+        std::string type;
+
+        std::string tooltip;
+
+        int priority;
+    };
+}
+#endif //TRIANGLE_H
