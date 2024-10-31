cf97c4263f6b93be904fa467b9c349fb547a0537
Dialpuri <dialpuri.jordan@gmail.com>
2024-10-31 14:41:01 +0000

    Added infrastructure for sails-morph start

---
+++ package/src/sails/morph.py
@@ -0,0 +1,32 @@
+import argparse
+from .__version__ import __version__
+import importlib
+from sails import morph, interface
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("--version", action="version", version=__version__)
+    parser.add_argument("-modelin", help="Path to input model", type=str, required=True)
+    parser.add_argument(
+        "-modelout", help="Path to output model", type=str, required=False
+    )
+    parser.add_argument("-chain", help="Name of target chain", type=str, required=True)
+    parser.add_argument(
+        "-res", help="Name of target residue (protein)", type=str, required=True
+    )
+    parser.add_argument(
+        "-wurcs", help="WURCS identifier for new glycan", type=str, required=True
+    )
+
+    return parser.parse_args()
+
+
+def run():
+    args = parse_args()
+
+    sails_structure = interface.get_sails_structure(args.modelin)
+    resource = importlib.resources.files("sails").joinpath("data")
+
+    morph(sails_structure, args.chain, args.res, str(resource))
