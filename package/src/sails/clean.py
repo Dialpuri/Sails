48853b594bd786ffc6ee7b904787be85e9cdbd9c
Jordan Dialpuri <44945647+Dialpuri@users.noreply.github.com>
2025-10-06 14:30:02 +0100

    Added machine learning prediction capability to Sails

---
+++ package/src/sails/clean.py
@@ -0,0 +1,73 @@
+#  Copyright (c) 2024 Jordan Dialpuri, Jon Agirre, Kathryn Cowtan, Paul Bond and University of York. All rights reserved
+
+import site
+import os
+
+
+def clean_models():
+    site_packages_dir = site.getsitepackages()
+
+    found_models = []
+
+    for folder in site_packages_dir:
+        sails_model_dir = os.path.join(folder, "sails_models")
+        if os.path.exists(sails_model_dir):
+            for model in os.scandir(sails_model_dir):
+                found_models.append(model)
+
+    if not found_models:
+        print("No models were found in site-packages.")
+        return
+
+    print("Pick an option to remove: ")
+
+    for index, model in enumerate(found_models):
+        print(f"{index + 1}) {model.name.removesuffix('.onnx')}")
+
+    if len(found_models) > 1:
+        print(f"{len(found_models) + 1}) All")
+
+    option_selected = False
+    while not option_selected:
+        option = input("Number: ")
+
+        try:
+            choice = int(option)
+            if choice <= 0 or choice > len(found_models) + 1:
+                raise ValueError()
+
+            if choice == len(found_models) + 1:
+                print("Do you want to remove all the models?")
+            else:
+                model_to_remove = found_models[choice - 1]
+                print(f"Confirm you want to remove {model_to_remove.name}?")
+
+            y_no_selected = False
+            confirm = False
+            while not y_no_selected:
+                y_or_n = input("Y/N ").lower()
+                if y_or_n not in ["y", "yes", "n", "no"]:
+                    continue
+
+                y_no_selected = True
+
+                if y_or_n == "y" or y_or_n == "yes":
+                    confirm = True
+
+            if confirm:
+                if choice == len(found_models) + 1:
+                    for model in found_models:
+                        os.remove(model.path)
+                        print("Removed", model.name)
+                else:
+                    model_to_remove = found_models[choice - 1]
+                    os.remove(model_to_remove.path)
+                    print("Removed", model_to_remove.name)
+
+            option_selected = True
+        except ValueError:
+            print("Invalid choice")
+
+
+def run():
+    clean_models()
---
+++ package/src/sails/install.py
@@ -0,0 +1,77 @@
+#  Copyright (c) 2024 Jordan Dialpuri, Jon Agirre, Kathryn Cowtan, Paul Bond and University of York. All rights reserved
+
+import site
+import os
+import argparse
+import enum
+from pathlib import Path
+from .__version__ import __version__
+from .logs import setup_logging
+from .prediction import model
+import logging
+
+
+class InstallLocation(enum.Enum):
+    site_packages = 0
+    ccp4 = 1
+
+
+def clibd_error_msg():
+    print("""CCP4 Environment Variable - CLIBD is not found.
+                You can try sourcing it:
+                Ubuntu - source /opt/xtal/ccp4-X.X/bin/ccp4.setup-sh
+                MacOS - source /Applications/ccp4-X.X/bin/ccp4.setup-sh
+                """)
+
+
+def install_model(type: model.ModelType, location: str, reinstall: bool) -> bool:
+    logging.info(f"Installing {type.name} model to {location}")
+    if InstallLocation[location] == InstallLocation.ccp4:
+        clibd = os.environ.get("CLIBD", "")
+        if not os.path.exists(clibd):
+            clibd_error_msg()
+            return False
+
+        model.download_model(type=type, folder=clibd, reinstall=reinstall)
+        return True
+
+    if InstallLocation[location] == InstallLocation.site_packages:
+        site_packages_dir = site.getsitepackages()
+        if not site_packages_dir:
+            raise RuntimeError(
+                "Failed to get site packages directory, ensure you in a virtual environment"
+            )
+        first_sitepackages = Path(site_packages_dir[0])
+        model.download_model(type=type, folder=first_sitepackages, reinstall=reinstall)
+        # download_database(folder=first_sitepackages, reinstall=reinstall)
+        return True
+    return False
+
+
+def run():
+    setup_logging()
+    output_list = ["ccp4", "site_packages"]
+
+    parser = argparse.ArgumentParser(description="nucleofind Install")
+    parser.add_argument(
+        "-m", "--model", choices=[type.name for type in model.ModelType], required=False
+    )
+    parser.add_argument(
+        "-o",
+        "--output",
+        choices=[location.name for location in InstallLocation],
+        required=False,
+        default=output_list[1],
+    )
+    parser.add_argument("--update", required=False, action="store_true")
+    parser.add_argument("-v", "--version", action="version", version=__version__)
+
+    args = parser.parse_args()
+
+    if not args.model:
+        print("Please specify a model you wish to download")
+        return
+
+    install_model(
+        type=model.ModelType[args.model], location=args.output, reinstall=args.update
+    )
